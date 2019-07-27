1a2be9a40adaf1af9601bf28f175d512bd761475
Aleksandra <ajmpawlik@gmail.com>
2019-07-27 18:54:33 +0100

    test: unit test for the docking station to respond to dock method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,8 +5,15 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
   it 'releases a bike'do
-    bike = subject.release_bike 
+    bike = subject.release_bike
     expect(bike).to be_working #the same as below, just different syntax
     expect(bike.working?).to be true
   end
+  it 'responds to dock method' do
+    expect(subject).to respond_to :dock
+  end
+  # it 'docks bike' do
+  #   bike = subject.release_bike
+  #
+  # end
 end
