d55f68fbde7582c3bd572bd3151f5f1cb140b460
Aleksandra <ajmpawlik@gmail.com>
2019-07-28 15:41:05 +0100

    test: raising_exception test implemented

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,7 +10,11 @@ describe DockingStation do
       subject.dock(bike)
       expect(subject.release_bike).to eq bike
     end
+    it 'raises an error when there are no bikes available' do
+      expect {subject.release_bike}.to raise_error 'No bikes available'
+    end
   end
+  
     it 'responds to dock method with one argument (bike)' do
       expect(subject).to respond_to(:dock).with(1).argument
     end
