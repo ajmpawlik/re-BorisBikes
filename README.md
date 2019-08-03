29fcdf6656c85985435d88f7fdc866dc62e2e8c6
Aleksandra <ajmpawlik@gmail.com>
2019-08-03 18:52:31 +0100

    refactor: introduces the default capacity

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # re-BorisBikes
 At this point I need to still review point 15 of the Makers BorisBikes challenge.
-And, the coverage of the SimpleCov should not be commited but it is - to be solved. 
+And, the coverage of the SimpleCov should not be commited but it is - to be solved.
+
+
+
+
+## User stories
+.
+.
+.
+As a system maintainer,
+So that busy areas can be served more effectively,
+I want to be able to specify a larger capacity when necessary.
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -26,11 +26,25 @@ describe DockingStation do
     end
 
     it 'raises an error when there are all spaces (1) taken by bikes in the docking station' do
-      20.times {subject.dock Bike.new}
+      subject.capacity.times { subject.dock Bike.new }
       expect {subject.dock Bike.new}.to raise_error 'Docking station full'
     end
   end
   it 'responds to method bike' do
     expect(subject).to respond_to(:bike)
+  end
+  it 'has a default capacity' do
+    expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
+  describe 'initialization' do
+    subject { DockingStation.new }
+    let(:bike) {Bike.new}
+    it 'defaults capacity' do
+      described_class::DEFAULT_CAPACITY.times do
+        subject.dock(bike)
+      end
+      expect{ subject.dock(bike). to raise_error 'Docking station full'}
+    end
   end
 end
